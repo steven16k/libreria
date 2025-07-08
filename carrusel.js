23d7a8bebc9e354b521db9dbb8954a8dd5821193
steven16k <105184641+steven16k@users.noreply.github.com>
2025-07-08 16:02:48 -0600

    primer commit

---
+++ carrusel.js
@@ -0,0 +1,17 @@
+import { carruselData } from "./carruselData.js";
+import { firstLetterUppercase } from "./stringFormatter.js";
+
+const slideTrack = document.querySelector(".slide__track");
+
+const slideTemplate = (data) => `
+  <div class="slide">
+    <div class="tag">${firstLetterUppercase(data.name)}</div>
+    <img src="${data.src}" alt="${data.name}">
+  </div>
+`;
+
+const carruselCards = [...carruselData, ...carruselData]
+  .map((data) => slideTemplate(data))
+  .join("");
+slideTrack.insertAdjacentHTML("beforeend", carruselCards);
+
---
+++ carruselData.js
@@ -0,0 +1,40 @@
+export const carruselData = [
+    {
+        name: "Solo Leveling",
+        src: "https://m.media-amazon.com/images/I/7142PfvTN8L._SL1500_.jpg",
+    },
+    {
+        name: "IT",
+        src: "https://m.media-amazon.com/images/I/71oBjdOSkHL._SL1500_.jpg",
+    },
+    {
+        name: "Fairy Tales",
+        src: "https://m.media-amazon.com/images/I/916I5JDDJ0L._SL1500_.jpg",
+    },
+    {
+        name: "The Walking Dead",
+        src: "https://m.media-amazon.com/images/I/81w2wDvI2KL._SL1500_.jpg",
+    },
+    {
+        name: "Blue Lock",
+        src: "https://otakuteca.com/images/books/cover/5d07c1d52967b.webp",
+    },
+    {
+        name: "Harry Potter y la Piedra Filosofal",
+        src: "https://images-na.ssl-images-amazon.com/images/I/81iqZ2HHD-L.jpg",
+    },
+    {
+        name: "El perfume: Historia de un asesino",
+        src: "https://m.media-amazon.com/images/I/91U30AzHZ-L._SL1500_.jpg",
+    },
+    {
+        name: "Guerra Mundial Z",
+        src: "https://m.media-amazon.com/images/I/71f32nD8PCL._SL1200_.jpg",
+
+    },
+    {
+    name: "Cicatrices Marcadas",
+    src: "https://m.media-amazon.com/images/I/612cqaTgyML._SL1500_.jpg",
+    },
+    
+];
